491a1018ff8cd65c0b514b7d582374273a152159
marmelatta <testandroidapp@yandex.ru>
2022-02-13 00:17:12 +0500

    добавлена аутентицикация

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
+const expressSession = require('express-session')
+const db = require('./models')
 
 // Routing
 const indexRouter = require('./routers/index');
 const advertisementRouter = require('./routers/advertisement')
+
+function verify (username, password, done) {
+    db.users.findByUsername(username,  function (err, user) {
+        if (err) { return done(err) }
+        if (!user) {return done(null, false) }
+
+        if (!db.users.verifyPassword(user,  password)) { return done(null, false) }
+
+        return done(null, user)
+    })
+}
+
+const options = {
+    usernameFiled: 'username',
+    passwordField: 'password',
+    passReqToCallback: false,
+}
+
+passport.use('local', new LocalStrategy(options, verify));
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id)
+})
+
+passport.deserializeUser(function (id, cb) {
+    db.users.findById(id,  function (err, user) {
+        if (err) {return cb(err) }
+        cb(null, user);
+    })
+})
 
 const app = express();
 
@@ -12,6 +46,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(expressSession({
+    secret: process.env.COOKIE_SECRET || 'dfkkf',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.use(passport.initialize(undefined));
+app.use(passport.session(undefined));
+
+//запросы
+app.get('',
+    function (req, res) {
+    console.log('после аутентификации переходит сюда')
+        res.json({user: req.user})
+    })
+
+app.get('/login',
+    function (req, res) {
+        res.json('login')
+    })
+
+app.post('/login',
+    passport.authenticate(
+        'local',
+        {
+            failureRedirect: '/login',
+        },
+    ),
+    function (req, res) {
+        console.log("req.user: ", req.user.toString())
+        res.redirect('/')
+    })
+
+app.get('/logout',
+    function (req, res) {
+        req.logout()
+        res.json(`dllsdluu`)
+    })
+
+app.get('/profile',
+    function (req, res, next) {
+        if (!req.isAuthenticated || !req.isAuthenticated()) {
+            if (req.session) {
+                req.session.returnTo = req.originalUrl || req.url
+            }
+            return res.redirect('/login')
+        }
+        next()
+    },
+    function (req, res) {
+        return res.json({ user: req.user })
+    })
 
 app.use('/', indexRouter);
 app.use('/public', express.static(__dirname + '/public'));
